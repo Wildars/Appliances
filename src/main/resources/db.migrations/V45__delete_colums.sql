713e269b7c010511c7520b65ea2855c387408df8
Wildars <80090254+Wildars@users.noreply.github.com>
2024-06-19 08:43:23 +0600

    fix

---
+++ src/main/resources/db.migrations/V45__delete_colums.sql
@@ -0,0 +1,9 @@
+alter table public.use
+drop column if exists procent_skidki cascade;
+
+alter table public.use
+drop column if exists type_client cascade;
+
+alter table public.use
+drop column if exists position cascade;
+
